6c225862b4f48e57bcef4fc9a0e780f8c28a6f30
Benny Powers <web@bennypowers.com>
2020-09-23 12:11:41 +0300

    chore: fix tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,23 @@
-import { rollup } from 'rollup';
+import { rollup, RollupOutput } from 'rollup';
 import { expect } from 'chai';
 import graphql from '../src';
 import { writeFileSync } from 'fs';
 import { unlinkSync } from 'fs';
 
+process.chdir(__dirname);
+
 // dynamic import() from a data uri doesn't work, and between ts and node,
 // import()ing the file didn't work either 🤷‍♂️
 /* eslint-disable @typescript-eslint/no-var-requires */
+
+async function generateCjsBundle(input: string): Promise<RollupOutput> {
+  return rollup({
+    input,
+    output: { exports: 'default' },
+    plugins: [graphql()],
+  })
+    .then(bundle => bundle.generate({ format: 'cjs' }));
+}
 
 describe('plugin', () => {
   afterEach(() => {
@@ -16,17 +27,11 @@ describe('plugin', () => {
   });
 
   it('should parse a simple graphql file', async function() {
-    const bundle = await rollup({
-      input: 'samples/basic/basic.graphql',
-      output: { exports: 'default' },
-      plugins: [graphql()],
-    });
-
-    const { output: [{ code }] } = await bundle.generate({ format: 'cjs' });
+    const { output: [{ code }] } = await generateCjsBundle('samples/basic/index.js');
 
     writeFileSync('./tmp.js', code, 'utf8');
 
-    const result = require('./tmp.js');
+    const { result } = require('./tmp.js');
 
     expect(result).to.be.ok;
     expect(result.kind, 'kind').to.equal('Document');
@@ -39,7 +44,8 @@ describe('plugin', () => {
     expect(result.loc.source).to.be.ok;
     expect(result.loc.source.body).to.equal('query GetHero{hero{id name __typename}}');
     const [{ selectionSet: { selections } }] = definition.selectionSet.selections;
-    // fragment field3
+
+    // fragment fields
     expect(selections.length).to.equal(3);
     expect(selections[0].name.value).to.equal('id');
     expect(selections[1].name.value).to.equal('name');
@@ -47,17 +53,11 @@ describe('plugin', () => {
   });
 
   it('should include a fragment', async function() {
-    const bundle = await rollup({
-      input: 'samples/fragments/allHeroesQuery.graphql',
-      output: { exports: 'default' },
-      plugins: [graphql()],
-    });
-
-    const { output: [{ code }] } = await bundle.generate({ format: 'cjs' });
+    const { output: [{ code }] } = await generateCjsBundle('samples/fragments/index.js');
 
     writeFileSync('./tmp.js', code, 'utf8');
 
-    const result = require('./tmp.js');
+    const { result } = require('./tmp.js');
 
     expect(result).to.be.ok;
     expect(result.kind, 'kind').to.equal('Document');
---
+++ test/samples/fragments/index.js
@@ -0,0 +1,2 @@
+import result from './Query.graphql';
+export { result }; 216.16;
