34d39d4948754b21d25bb3dedb797e6bc6c53c66
Benny Powers <web@bennypowers.com>
2020-09-23 11:42:29 +0300

    chore: lint

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
 const pkg = require('./package.json');
 
 export default {
-	input: 'src/index.js',
-	output: [
-		{ file: pkg.main, format: 'cjs' },
-		{ file: pkg.module, format: 'es' }
-	],
-	external: Object.keys(pkg.dependencies),
-	sourceMap: true
+  external: Object.keys(pkg.dependencies),
+  sourceMap: true,
+  input: 'src/index.js',
+  output: [
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'es' }
+  ],
 };
